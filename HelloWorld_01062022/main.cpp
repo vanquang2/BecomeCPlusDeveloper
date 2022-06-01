31f4addf63607ca1e57449e8b3070a76b784b3a7
quangnv.aladintech <quangnv@aladintech.co>
2022-06-01 17:38:45 +0700

    01/06/2022 Learn C++

---
+++ HelloWorld_01062022/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>; //Chi thi tien xy ly
+
+using namespace std;
+
+int main(){
+	 //Cau lenh gan
+	int nSoNguyen;
+	
+	 // Cau lenh khai bao
+	nSoNguyen = 70;
+	
+	 //Bieu thuc
+	nSoNguyen = (5 + 10) * (10 - 5);
+	
+	 // Cau lenh loi goi ham
+	 /*
+	 Comment nhieu dong
+	 */
+	cout << "Hello World" << endl;
+	system("pause");
+	return 0; //tra ve gia tri 0 cho ham main
+}
+
+//Thu vien : tap hop nhung ma duoc bien dich san, duoc dong goi lai de lap trinh vien tai su dung va khong phai viet lai moi khi su dung
---
+++ Variables_01062022/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	string name;
+	cout << "Ten cua ban" << endl;
+	cin >> name;
+	cout << "Hello "<<name;
+	return 0;
+}
+
